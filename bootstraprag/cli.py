3bbfe5aa76be9108929a121c500d42fe1b55a46b
pavanmantha <pavan.mantha@thevaslabs.io>
2024-07-27 04:07:06 +0530

    -refactoring, -new feature like observability

--- bootstraprag/cli.py
+++ bootstraprag/cli.py
@@ -17,20 +17,28 @@ def create_zip(project_name):
 
 @click.command()
 @click.argument('project_name')
-@click.option('--technology', type=click.Choice(['llamaindex', 'langchain', 'haystack']),
+@click.option('--framework', type=click.Choice(['llamaindex', 'langchain', 'haystack', 'None']),
               prompt="Which technology would you like to use?")
 @click.option('--template', type=click.Choice([]), prompt=False)
-def create(project_name, technology, template):
-    if technology == 'llamaindex' or technology == 'langchain' or technology == 'haystack':
+@click.option('--observability', type=click.Choice([]), prompt=False)
+def create(project_name, framework, template, observability):
+    template_choices = []
+    observability_choices = []
+
+    if framework == 'llamaindex' or framework == 'langchain' or framework == 'haystack':
         template_choices = ['simple-rag', 'self-rag', 'rag-with-cot', 'rag-with-ReACT', 'rag-with-HyDE']
-    elif technology == 'qdrant':
-        template_choices = ['simple-rag', 'self-rag', 'rag-with-cot', 'rag-with-ReACT', 'rag-with-HyDE']
+    elif framework == 'None':
+        template_choices = ['simple-search', 'hybrid-search']
 
     template = click.prompt("Which template would you like to use?",
                             type=click.Choice(template_choices)
                             )
-
-    click.echo(f'You have selected technology: {technology} and template: {template}')
+    if framework == 'llamaindex' or framework == 'langchain' or framework == 'haystack':
+        observability_choices = ['Yes', 'No']
+        observability = click.prompt("Do you wish to enable observability?",
+                                     type=click.Choice(observability_choices)
+                                     )
+    click.echo(f'You have selected framework: {framework} and template: {template} and observability: {observability}')
 
 
 cli.add_command(create)
---
+++ bootstraprag/templates/llamaindex/rag_with_hyde_observability/readme.md
@@ -0,0 +1,6 @@
+## Instructions to run the code
+
+- Navigate to the root of the project and run the below command
+- `pip install -r requirements.txt`
+- In the data folder place your data preferably any ".pdf"
+- run `python main.py`
