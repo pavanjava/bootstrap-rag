69e8cc6f74295b22da684dcaeb44f28fd04450be
pavanmantha <pavan.mantha@thevaslabs.io>
2024-07-26 23:07:09 +0530

    major refactoring on cli

--- bootstraprag/cli.py
+++ bootstraprag/cli.py
@@ -17,33 +17,20 @@ def create_zip(project_name):
 
 @click.command()
 @click.argument('project_name')
-@click.option('--template', type=click.Choice(['simple-search', 'hybrid-search', 'llamaindex-rag', 'rag-with-cot', 'rag-with-ReACT', 'rag-with-HyDE']),
-              prompt="Which template would you like to use?")
-@click.option('--technology', type=click.Choice(['llamaindex', 'langchain', 'haystack', 'qdrant']),
+@click.option('--technology', type=click.Choice(['llamaindex', 'langchain', 'haystack']),
               prompt="Which technology would you like to use?")
-@click.option('--observability', type=click.Choice(['Yes', 'No']), prompt="Would you like to set up observability?")
-@click.option('--api_key', prompt="Please provide your OpenAI API key (leave blank to skip)", default='',
-              required=False)
-@click.option('--data_source', type=click.Choice(['PDF', 'TXT']),
-              prompt="Which data source would you like to use?")
-@click.option('--vector_db', type=click.Choice(['Yes', 'No']), prompt="Would you like to use a vector database?")
-def create(project_name, template, technology, observability, api_key, data_source, vector_db):
-    """Creates a new project with the specified type."""
-    template_path = Path(__file__).parent / 'templates' / technology / template
-    project_path = Path.cwd() / project_name
+@click.option('--template', type=click.Choice([]), prompt=False)
+def create(project_name, technology, template):
+    if technology == 'llamaindex' or technology == 'langchain' or technology == 'haystack':
+        template_choices = ['simple-rag', 'self-rag', 'rag-with-cot', 'rag-with-ReACT', 'rag-with-HyDE']
+    elif technology == 'qdrant':
+        template_choices = ['simple-rag', 'self-rag', 'rag-with-cot', 'rag-with-ReACT', 'rag-with-HyDE']
 
-    if project_path.exists():
-        click.echo(f"Error: Project directory {project_name} already exists!")
-        return
+    template = click.prompt("Which template would you like to use?",
+                            type=click.Choice(template_choices)
+                            )
 
-    shutil.copytree(template_path, project_path)
-
-    # Optionally handle the API key and other parameters here if necessary
-
-    zip_path = create_zip(project_name)
-
-    click.echo(f"Created {template} project at {project_path}")
-    click.echo(f"Project archived as {zip_path}")
+    click.echo(f'You have selected technology: {technology} and template: {template}')
 
 
 cli.add_command(create)
