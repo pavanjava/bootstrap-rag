01ce0308d58f6f5a8018c39269eb7010f1264edb
Kameshwara Pavan Kumar Mantha <25398886+pavanjava@users.noreply.github.com>
2024-07-27 05:01:25 +0530

    Refactoring (#1)
    * major refactoring on cli
    
    * technology scaffolding refactoring
    
    * -refactoring, -new feature like observability
    
    * -major refactoring, -added arize phoenix observability
    
    ---------
    
    Co-authored-by: pavanmantha <pavan.mantha@thevaslabs.io>

--- bootstraprag/cli.py
+++ bootstraprag/cli.py
@@ -17,33 +17,50 @@ def create_zip(project_name):
 
 @click.command()
 @click.argument('project_name')
-@click.option('--template', type=click.Choice(['simple-search', 'hybrid-search', 'llamaindex-rag', 'rag-with-cot', 'rag-with-ReACT', 'rag-with-HyDE']),
-              prompt="Which template would you like to use?")
-@click.option('--technology', type=click.Choice(['llamaindex', 'langchain', 'haystack', 'qdrant']),
+@click.option('--framework', type=click.Choice(['llamaindex', 'langchain', 'haystack', 'None']),
               prompt="Which technology would you like to use?")
-@click.option('--observability', type=click.Choice(['Yes', 'No']), prompt="Would you like to set up observability?")
-@click.option('--api_key', prompt="Please provide your OpenAI API key (leave blank to skip)", default='',
-              required=False)
-@click.option('--data_source', type=click.Choice(['PDF', 'TXT']),
-              prompt="Which data source would you like to use?")
-@click.option('--vector_db', type=click.Choice(['Yes', 'No']), prompt="Would you like to use a vector database?")
-def create(project_name, template, technology, observability, api_key, data_source, vector_db):
-    """Creates a new project with the specified type."""
-    template_path = Path(__file__).parent / 'templates' / technology / template
+@click.option('--template', type=click.Choice([]), prompt=False)
+@click.option('--observability', type=click.Choice([]), prompt=False)
+def create(project_name, framework, template, observability):
+    template_choices = []
+    observability_choices = []
+
+    if framework == 'llamaindex' or framework == 'langchain' or framework == 'haystack':
+        template_choices = ['simple-rag', 'self-rag', 'rag-with-cot', 'rag-with-react', 'rag-with-hyde']
+    elif framework == 'None':
+        framework = 'qdrant'
+        template_choices = ['simple-search', 'hybrid-search']
+
+    template = click.prompt("Which template would you like to use?",
+                            type=click.Choice(template_choices)
+                            )
+    if framework == 'llamaindex' or framework == 'langchain' or framework == 'haystack':
+        observability_choices = ['Yes', 'No']
+        observability = click.prompt("Do you wish to enable observability?",
+                                     type=click.Choice(observability_choices)
+                                     )
+
+    click.echo(f'You have selected framework: {framework} and template: {template} and observability: {observability}')
+    download_and_extract_template(project_name, framework, template, observability)
+
+
+def download_and_extract_template(project_name, framework, template, observability_selection):
+    if observability_selection == 'Yes':
+        folder_name = template.replace('-', '_') + '_observability'
+        base_path = Path(__file__).parent / 'templates' / framework / folder_name
+    else:
+        base_path = Path(__file__).parent / 'templates' / framework / str(template).replace('-', '_')
     project_path = Path.cwd() / project_name
 
     if project_path.exists():
         click.echo(f"Error: Project directory {project_name} already exists!")
         return
 
-    shutil.copytree(template_path, project_path)
-
-    # Optionally handle the API key and other parameters here if necessary
-
-    zip_path = create_zip(project_name)
-
-    click.echo(f"Created {template} project at {project_path}")
-    click.echo(f"Project archived as {zip_path}")
+    try:
+        shutil.copytree(base_path, project_path)
+        click.echo(f"Project {project_name} created successfully at {project_path}")
+    except Exception as e:
+        click.echo(f"Error: {e}")
 
 
 cli.add_command(create)
---
+++ bootstraprag/templates/llamaindex/rag_with_hyde_observability/readme.md
@@ -0,0 +1,9 @@
+## Instructions to run the code
+
+- Navigate to the root of the project and run the below command
+- `pip install -r requirements.txt`
+- In the data folder place your data preferably any ".pdf"
+- change the data directory in `main.py`
+#### Note: ensure your qdrant and ollama (if LLM models are pointing to local) are running
+- run `python main.py`
+- visit http://localhost:6006/ for all the observability
