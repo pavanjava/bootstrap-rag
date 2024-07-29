1b2592292e7dbf838d43ef25eecfa0538766ae02
pavanmantha <pavan.mantha@thevaslabs.io>
2024-07-29 17:49:35 +0530

    Major feature addition 'llamaindex react agent for rag'

---
+++ bootstraprag/templates/llamaindex/rag_with_react/main.py
@@ -0,0 +1,16 @@
+# driver code
+from react_agent_with_query_engine import ReActWithQueryEngine
+
+react_with_engine = ReActWithQueryEngine(input_dir='<YOUR_DATA_FOLDER>')
+
+# Start a loop to continually get input from the user
+while True:
+    # Get a query from the user
+    user_query = input("Enter your query [type 'bye' to 'exit']: ")
+
+    # Check if the user wants to terminate the loop
+    if user_query.lower() == "bye" or user_query.lower() == "exit":
+        break
+
+    response = react_with_engine.query(user_query=user_query)
+    print(response)
