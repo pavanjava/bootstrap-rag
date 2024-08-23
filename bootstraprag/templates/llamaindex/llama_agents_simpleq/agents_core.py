05618d88b671673ada31d8340b06f9c8a7b390b1
Kameshwara Pavan Kumar Mantha <25398886+pavanjava@users.noreply.github.com>
2024-08-23 22:03:45 +0530

    Simpleq llama agents (#10)
    * new feature of llama-agents
    
    * modified the code for agents as class object
    
    ---------
    
    Co-authored-by: pavanmantha <pavan.mantha@thevaslabs.io>

--- bootstraprag/templates/llamaindex/llama_agents_simpleq/agents_core.py
+++ bootstraprag/templates/llamaindex/llama_agents_simpleq/agents_core.py
@@ -5,9 +5,18 @@ from llama_agents import (
     SimpleMessageQueue,
 )
 
+from llama_index.core import Settings
 from llama_index.core.agent import ReActAgent
 from llama_index.core.tools import FunctionTool
-from llama_index.llms.openai import OpenAI
+from llama_index.llms.ollama import Ollama
+from llama_index.embeddings.ollama import OllamaEmbedding
+
+
+llm = Ollama(model=os.environ['OLLAMA_LLM_MODEL'], base_url=os.environ['OLLAMA_BASE_URL'], request_timeout=300)
+embed_model = OllamaEmbedding(model_name=os.environ['OLLAMA_EMBED_MODEL'], base_url=os.environ['OLLAMA_BASE_URL'])
+
+Settings.llm = llm
+Settings.embed_model = embed_model
 
 
 # create an agent
@@ -15,30 +24,30 @@ def get_the_secret_fact() -> str:
     """Returns the secret fact."""
     return "The secret fact is: A baby llama is called a 'Cria'."
 
+class SimpleQAgent:
+    tool = FunctionTool.from_defaults(fn=get_the_secret_fact)
 
-tool = FunctionTool.from_defaults(fn=get_the_secret_fact)
+    agent1 = ReActAgent.from_tools([tool], llm=Settings.llm)
+    agent2 = ReActAgent.from_tools([], llm=Settings.llm)
 
-agent1 = ReActAgent.from_tools([tool], llm=OpenAI())
-agent2 = ReActAgent.from_tools([], llm=OpenAI())
-
-# create our multi-agent framework components
-message_queue = SimpleMessageQueue(port=8000)
-control_plane = ControlPlaneServer(
-    message_queue=message_queue,
-    orchestrator=AgentOrchestrator(llm=OpenAI(model="gpt-4-turbo")),
-    port=8001,
-)
-agent_server_1 = AgentService(
-    agent=agent1,
-    message_queue=message_queue,
-    description="Useful for getting the secret fact.",
-    service_name="secret_fact_agent",
-    port=8002,
-)
-agent_server_2 = AgentService(
-    agent=agent2,
-    message_queue=message_queue,
-    description="Useful for getting random dumb facts.",
-    service_name="dumb_fact_agent",
-    port=8003,
-)
+    # create our multi-agent framework components
+    message_queue = SimpleMessageQueue(port=8000)
+    control_plane = ControlPlaneServer(
+        message_queue=message_queue,
+        orchestrator=AgentOrchestrator(llm=Settings.llm),
+        port=8001,
+    )
+    agent_server_1 = AgentService(
+        agent=agent1,
+        message_queue=message_queue,
+        description="Useful for getting the secret fact.",
+        service_name="secret_fact_agent",
+        port=8002,
+    )
+    agent_server_2 = AgentService(
+        agent=agent2,
+        message_queue=message_queue,
+        description="Useful for getting random dumb facts.",
+        service_name="dumb_fact_agent",
+        port=8003,
+    )
