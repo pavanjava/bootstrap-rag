4171e9d42a17b78d4d15d31db540c8bcbe70ddda
pavanmantha <pavan.mantha@thevaslabs.io>
2024-08-23 22:00:38 +0530

    modified the code for agents as class object

--- bootstraprag/templates/llamaindex/llama_agents_simpleq/main.py
+++ bootstraprag/templates/llamaindex/llama_agents_simpleq/main.py
@@ -1,15 +1,17 @@
 from llama_agents import LocalLauncher
 import nest_asyncio
-from agents_core import agent_server_1, agent_server_2, control_plane, message_queue
+from agents_core import SimpleQAgent
 
+
+simple_agent = SimpleQAgent()
 # needed for running in a notebook
 nest_asyncio.apply()
 
 # launch it
 launcher = LocalLauncher(
-    [agent_server_1, agent_server_2],
-    control_plane,
-    message_queue,
+    [simple_agent.agent_server_1, simple_agent.agent_server_2],
+    simple_agent.control_plane,
+    simple_agent.message_queue,
 )
 result = launcher.launch_single("What is the secret fact?")
 
