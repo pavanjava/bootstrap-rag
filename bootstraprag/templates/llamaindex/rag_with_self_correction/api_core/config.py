5d72115b406069e7fd1274d2d48dec271261c47c
pavanmantha <pavan.mantha@thevaslabs.io>
2024-09-22 19:02:33 +0530

    -implemented fastapi for self correcting rag

---
+++ bootstraprag/templates/llamaindex/rag_with_self_correction/api_core/config.py
@@ -0,0 +1,5 @@
+class Settings:
+    PROJECT_NAME: str = "Simple RAG as FastAPI Application"
+
+
+settings = Settings()
--- bootstraprag/templates/llamaindex/simple_rag_with_observability/apis.py
+++ bootstraprag/templates/llamaindex/simple_rag_with_observability/apis.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.openapi.utils import get_openapi
 from api_routes.apis import router
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
+import logging
+import time
+
+logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
+logger = logging.getLogger(__name__)
+allowed_origins = [
+    "*"
+]
 
 app = FastAPI(
     title="My FastAPI Application",
@@ -20,7 +33,13 @@ app = FastAPI(
     },
     terms_of_service="https://www.yourwebsite.com/terms/",
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 
 
@@ -47,9 +66,42 @@ def custom_openapi():
 
 app.openapi = custom_openapi
 
+
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    try:
+        logger.info(f"Incoming request: {request.method} {request.url}")
+        response = await call_next(request)
+        logger.info(f"Response status: {response.status_code}")
+        return response
+    except Exception as e:
+        logger.exception(f"Error processing request: {e}")
+        raise e
+
+
+# Request Timing Middleware
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    logger.info(f"Processed in {process_time:.4f} seconds")
+    return response
+
+
+# Logging Middleware
+@app.middleware("http")
+async def log_requests(request: Request, call_next):
+    logger.info(f"Incoming request: {request.method} {request.url}")
+    response = await call_next(request)
+    logger.info(f"Response status: {response.status_code}")
+    return response
+
+
 if __name__ == "__main__":
     uvicorn.run(
-        "main:app",
+        "apis:app",
         host="127.0.0.1",
         port=8000,
         reload=True,
