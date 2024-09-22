0efc888083a72622309eb4f8ab735a3ba84c1510
pavanmantha <pavan.mantha@thevaslabs.io>
2024-09-22 19:12:50 +0530

    -deleted unwanted code

--- bootstraprag/templates/llamaindex/llama_deploy_with_kafka/workflows/events.py
+++
@@ -1,9 +0,0 @@
-from llama_index.core.workflow import Event
-from llama_index. core. base. base_query_engine import BaseQueryEngine
-
-
-class QueryEngineEvent(Event):
-    """Result of running retrieval"""
-
-    base_query_engine: BaseQueryEngine
-
