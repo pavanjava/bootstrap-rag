1ee5eaf673a635f3ced06aeae3ad5f1784c89652
pavanmantha <pavan.mantha@thevaslabs.io>
2024-09-24 06:48:32 +0530

    -modified the configs accordingly

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
+import pathlib
+
+doc_path = pathlib.Path(__file__).parent.resolve()
+long_description = (doc_path / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='bootstrap-rag',
-    version='0.0.4',
+    version='0.0.5',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
