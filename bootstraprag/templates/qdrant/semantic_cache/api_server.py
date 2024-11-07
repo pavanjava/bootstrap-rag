e1b0f38a0a0ae11dbf33b892be27ef032863d863
Kameshwara Pavan Kumar Mantha <25398886+pavanjava@users.noreply.github.com>
2024-11-07 22:02:06 +0530

    -added skeliton code for litserve (#77)
    Co-authored-by: pavanmantha <pavan.mantha@thevaslabs.io>

---
+++ bootstraprag/templates/qdrant/semantic_cache/api_server.py
@@ -0,0 +1,19 @@
+from abc import ABC
+import litserve as ls
+
+
+class SemanticCacheAPI(ls.LitAPI, ABC):
+    def __init__(self):
+        pass
+
+    def setup(self, device):
+        pass
+
+    def decode_request(self, request, **kwargs):
+        pass
+
+    def predict(self, x, **kwargs):
+        pass
+
+    def encode_response(self, output, **kwargs):
+        pass
